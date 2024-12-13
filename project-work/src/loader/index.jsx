d169ced700a540be148ec5d27fce12163fd3a3e5
AbekD <adancho2009@gmail.com>
2024-12-14 00:42:06 +0500

    фиксирование фона

--- project-work/src/loader/index.jsx
+++ project-work/src/loader/index.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 const Loader = () => {
-	return (
-		<div>
-			<div className='container'>
-				<div className='loader elements'></div>
-				<div className='loader elements'></div>
-				<div className='loader elements'></div>
-			</div>
-		</div>
-	)
+	return <div></div>
 }
 
 export default Loader
