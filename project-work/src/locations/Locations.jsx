e468033e5b6b39e53a78fce0b566d6cd12cbbed2
AbekD <adancho2009@gmail.com>
2024-12-13 19:16:30 +0500

    добавление фона для локации

--- project-work/src/locations/Locations.jsx
+++ project-work/src/locations/Locations.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Loader from '@/loader'
 import { useNavigate } from 'react-router-dom'
+import './Location.css'
 
 export default function Locations() {
 	const [locations, setLocations] = useState([])
@@ -17,16 +18,6 @@ export default function Locations() {
 		fetchLocations(page)
 	}, [page])
 
-	const fetchCharacterNames = async urls => {
-		try {
-			const responses = await Promise.all(urls.map(url => axios.get(url)))
-			return responses.map(response => response.data.name) // Возвращаем только имена
-		} catch (error) {
-			console.error('Failed to fetch character names:', error)
-			return []
-		}
-	}
-
 	const fetchLocations = async currentPage => {
 		if (isLoading || page > totalPages) return
 		setIsLoading(true)
@@ -37,7 +28,7 @@ export default function Locations() {
 			)
 			const newLocations = response.data.results.map(location => ({
 				...location,
-				residentCount: location.residents.length, // Считаем количество резидентов
+				residentCount: location.residents.length,
 			}))
 
 			setLocations(prev => [...prev, ...newLocations])
@@ -52,7 +43,7 @@ export default function Locations() {
 
 	const handleScroll = () => {
 		const scrollPosition = window.innerHeight + window.scrollY
-		const threshold = document.documentElement.offsetHeight - 100 // Adjust threshold as needed
+		const threshold = document.documentElement.offsetHeight - 100
 
 		if (scrollPosition >= threshold && !isLoading) {
 			setPage(prev => prev + 1)
@@ -75,52 +66,54 @@ export default function Locations() {
 
 	return (
 		<React.Fragment>
-			<div className='my-4 text-center'>
-				<input
-					type='text'
-					placeholder='Search characters...'
-					value={searchQuery}
-					onChange={e => setSearchQuery(e.target.value)}
-					className='border-[1px] rounded px-2 py-1 w-[300px]'
-				/>
-			</div>
-			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-				{filteredLocations.length > 0 ? (
-					filteredLocations.map(location => (
-						<div
-							key={location.id} // Уникальный ключ
-							className='w-[200px] h-[200px] bg-slate-300 flex items-center justify-center'
-						>
-							<div className='p-4'>
-								<h2 className='text-lg font-semibold text-gray-800'>
-									{location.id}. {location.name}
-								</h2>
-								<p className='text-sm text-gray-600 mt-2'>
-									<span className='font-bold'>Type:</span> {location.type}
-								</p>
-								<p className='text-sm text-gray-600'>
-									<span className='font-bold'>Dimension:</span>{' '}
-									{location.dimension}
-								</p>
-								<p className='text-sm text-gray-600'>
-									<span className='font-bold'>Residents:</span>{' '}
-									<button
-										onClick={() => navigate(`/residents/${location.id}`)}
-										className='text-blue-500 underline'
-									>
-										{location.residentCount}
-									</button>
-								</p>
+			<div className='locations-Bg'>
+				<div className='search-bar'>
+					<input
+						type='text'
+						placeholder='Search locations...'
+						value={searchQuery}
+						onChange={e => setSearchQuery(e.target.value)}
+						className='search-style'
+					/>
+				</div>
+				<div className='location-Container'>
+					{filteredLocations.length > 0 ? (
+						filteredLocations.map(location => (
+							<div
+								key={location.id} // Уникальный ключ
+								className='location-card'
+							>
+								<div className='location-info'>
+									<h2 className='location-name'>
+										{location.id}. {location.name}
+									</h2>
+									<p className='location-key'>
+										<span className='font-bold'>Type:</span> {location.type}
+									</p>
+									<p className='location-key'>
+										<span className='font-bold'>Dimension:</span>{' '}
+										{location.dimension}
+									</p>
+									<p className='location-key'>
+										<span className='font-bold'>Count of Residents:</span>{' '}
+										<button
+											onClick={() => navigate(`/residents/${location.id}`)}
+											className='to-residents'
+										>
+											{location.residentCount}
+										</button>
+									</p>
+								</div>
 							</div>
-						</div>
-					))
-				) : (
-					<p className='text-center w-full'>No locations found</p>
-				)}
+						))
+					) : (
+						<p className='locations-no'>No locations found</p>
+					)}
+				</div>
 			</div>
 
 			{isLoading && (
-				<div className='text-center mt-4'>
+				<div className='loader'>
 					<Loader />
 				</div>
 			)}
